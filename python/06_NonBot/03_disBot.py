bd3904dde2f1753cc3f4056e833e1357e3592c32
Jarvis_N <non.ny1999@hotmail.com>
2023-03-31 14:10:41 +0700

    update : 06 merga with 03

--- python/06_NonBot/03_disBot.py
+++ python/06_NonBot/03_disBot.py
@@ -32,48 +32,27 @@ async def ping(interaction: nextcord.Interaction):
 #     await interaction.send(f'What do you want to order? : {data1}',ephemeral=True)
 
 @bot.slash_command(name="command")
-async def order(interaction: nextcord.Interaction, ticker: str, start: str, end: str, unit: str , num: str):
+async def order(interaction: nextcord.Interaction, ticket: str, dayStart: str, dayStop: str, unit: str):
 
-    print(ticker)
-    print(type(ticker))
-    print(start)
-    print(type(start))
-    print(end)
-    print(unit)
-
-
-    await interaction.send(f'Please wait', ephemeral=True)
-    res = c.get_aggs(f"C:{ticker}, {num}, {unit}, {start}, {end}")
-    # res = c.get_aggs("C:XAUUSD", 1, "day", "2023-03-22", "2023-03-24")
-    print(res)
-
-
-    await interaction.send(f'{res}', ephemeral=True)
-    await interaction.send(f'Done', ephemeral=True)
-
-
-@bot.command(name="qwerty")
-async def non(aaa):
-
-    res = c.get_aggs("C:XAUUSD", 1, "day", "2023-03-22", "2023-03-24")
-    print(res)
+    # get response
+    res = c.get_aggs(f"C:{ticket}", 1, f"{unit}", f"{dayStart}", f"{dayStop}")
 
     # create list of dictionaries containing data for each time period
-    data = [{'Open': item.open, 'High': item.high, 'Low': item.low, 'Close': item.close, 'Volume': item.volume,
-            'VWAP': item.vwap, 'Timestamp': item.timestamp, 'Transactions': item.transactions} for item in res]
+    data = [{'Open': item.open, 'High': item.high, 'Low': item.low, 'Close': item.close, 'Volume': item.volume, 'VWAP': item.vwap,
+             'Timestamp': pd.to_datetime(item.timestamp/1000, unit='s'), 'Transactions': item.transactions} for item in res]
 
     # create DataFrame from list of dictionaries
     df = pd.DataFrame(data)
 
-    # print the resulting DataFrame
-    print(df)
-
+    # Convert the DataFrame to a string
+    df_string = df.to_string()
 
     # Format the DataFrame using tabulate
     table = tabulate(df, headers='keys', tablefmt='psql')
 
-    # Send the table as a message using Nextcord
-    await aaa.send(f'```\n{table}\n```', ephemeral=True)
+    print(f'```\n{table}\n```')
+    
+    await interaction.send(f'```\n{table}\n```')
 
 
 @bot.slash_command(name="non")
@@ -107,4 +86,4 @@ async def on_ready():
 
 
 if __name__ == '__main__':
-    bot.run('MTA4OTc5ODk2NDgxNzE3ODc0NQ.GgUZW5._dJUD8p0q7TGn2upX9ybiygUfj5UPGrwu0dJkg')
+    bot.run('MMTA4OTc5ODk2NDgxNzE3ODc0NQ.GkLCVv.EfR8otdXaLYdRSnHMjlMNRKnLoyPWydQvq72zs')
--- python/06_NonBot/06_testInput.py
+++ python/06_NonBot/06_testInput.py
@@ -1,25 +1,28 @@
 import pandas as pd
+from datetime import datetime
 from polygon import RESTClient
-
 from tabulate import tabulate
 
 # create client
 c = RESTClient(api_key="sgI3UObKfOdY6Esk6_Z77g6rfnPpUgZx")
 
-# get response
-res = c.get_aggs("C:XAUUSD", 1, "day", "2023-03-22", "2023-03-24")
+ticket = input("Ticket : ").upper()
+unit = input("Unit : ")
+dayStart = input("Day : ")
+dayStop = input("Day (leave blank for current time) : ")
 
-# print(res)
+# set current time if dayStop is not provided
+if dayStop == "":
+    dayStop = datetime.now().strftime("%Y-%m-%d")
+
+# get response
+res = c.get_aggs(f"C:{ticket}", 1, f"{unit}", f"{dayStart}", f"{dayStop}")
 
 # create list of dictionaries containing data for each time period
-data = [{'Open': item.open, 'High': item.high, 'Low': item.low, 'Close': item.close, 'Volume': item.volume, 'VWAP': item.vwap, 'Timestamp': pd.to_datetime(item.timestamp/1000, unit='s'), 'Trade_ID': item.transactions} for item in res]
+data = [{'Open': item.open, 'High': item.high, 'Low': item.low, 'Close': item.close, 'Volume': item.volume, 'VWAP': item.vwap, 'Timestamp': pd.to_datetime(item.timestamp/1000, unit='s'), 'Transactions': item.transactions} for item in res]
 
 # create DataFrame from list of dictionaries
 df = pd.DataFrame(data)
-
-# print the resulting DataFrame
-# print(df)
-
 
 # Convert the DataFrame to a string
 df_string = df.to_string()
