0351fed1fa0f5b92809398e6265fd0e224ee558b
Jarvis_N <non.ny1999@hotmail.com>
2023-04-03 22:23:20 +0700

    Update : Can ask price NonBot

--- python/06_NonBot/07_flask.py
+++ python/06_NonBot/07_flask.py
@@ -1,6 +1,10 @@
 from flask import Flask,jsonify,request
-
+import pandas as pd
+from polygon import RESTClient
 import socket
+from tabulate import tabulate
+import datetime
+
 
 app = Flask(__name__)
 
@@ -17,6 +21,53 @@ def myapi():
     print(data)
     # process the data as needed
     return 'Success'
+
+
+
+
+@app.route('/ask', methods=['POST'])
+def price():
+    
+    data = request.get_json()
+    print(data)
+    print("\n ================================ \n")
+    
+    ticket = data['Ticket']
+    day_start = data['DayStart']
+    day_stop = data['DayStop']
+    unit = data['UniT']
+    
+    # Check if day_start has a year
+    if len(day_start) < 6:
+        current_year = datetime.datetime.now().year
+        day_start = f"{current_year}-{day_start}"
+
+    # Check if day_stop has a year
+    if len(day_stop) < 6:
+        current_year = datetime.datetime.now().year
+        day_stop = f"{current_year}-{day_stop}"
+        
+    
+    
+    # create client
+    c = RESTClient(api_key="sgI3UObKfOdY6Esk6_Z77g6rfnPpUgZx")
+
+    # get response
+    res = c.get_aggs(f'C:{ticket}', 1, f'{unit}', f'{day_start}', f'{day_stop}')
+
+    # create list of dictionaries containing data for each time period
+    data = [{'Open': item.open, 'High': item.high, 'Low': item.low, 'Close': item.close, 'Volume': item.volume, 'VWAP': item.vwap, 'Timestamp': pd.to_datetime(item.timestamp/1000, unit='s'), 'Transactions': item.transactions} for item in res]
+
+    # create DataFrame from list of dictionaries
+    df = pd.DataFrame(data)
+    
+    # Format the DataFrame using tabulate
+    table = tabulate(df, headers='keys', tablefmt='psql')
+    
+    print(f'```\n{table}\n```')
+    non = f'```\n{table}\n```'
+    
+    return non
 
 if __name__ == "__main__":
     app.run()
--- python/06_NonBot/43_disBot.py
+++ python/06_NonBot/43_disBot.py
@@ -3,6 +3,7 @@ from nextcord.ext import commands
 import requests
 import json
 
+
 Token = 'MTA4OTc5ODk2NDgxNzE3ODc0NQ.G5L-W3.Lk54axG2t9tQSd3CzYEQE6hVcrsh4k-c87SwmQ'
 
 
@@ -19,45 +20,102 @@ async def ping(interaction: nextcord.Interaction):
 
     await interaction.send("Pong4!", ephemeral=True)
 
+# ==================== Test Zone : Start ====================
 
-@bot.slash_command(name="aaa")
-async def non1(interaction: nextcord.Interaction, data1: str, data2: str):
+# @bot.slash_command(name="aaa")
+# async def non1(interaction: nextcord.Interaction, data1: str, data2: str):
 
-    await interaction.send(f"ok aaa!", ephemeral=True)
+#     await interaction.send(f"ok aaa!", ephemeral=True)
 
-@bot.slash_command(name="bbb")
-async def non2(interaction: nextcord.Interaction, data1: str):
+# @bot.slash_command(name="bbb")
+# async def non2(interaction: nextcord.Interaction, data1: str):
 
-    await interaction.send(f"ok bbb!", ephemeral=True)
-
-
-@bot.slash_command(name="ddd")
-async def non2(interaction: nextcord.Interaction, data3: str):
-    await interaction.send(f'test')
-    # await interaction.send(f"ok ddd!", ephemeral=True)
-
-@bot.slash_command(name="eee")
-async def non2(interaction: nextcord.Interaction, data3: str):
+#     await interaction.send(f"ok bbb!", ephemeral=True)
 
 
-    payload = {'key1': 'value1', 'key2': 'value2'}
+# @bot.slash_command(name="ddd")
+# async def non2(interaction: nextcord.Interaction, data3: str):
+#     await interaction.send(f'test')
+#     # await interaction.send(f"ok ddd!", ephemeral=True)
+
+# @bot.slash_command(name="eee")
+# async def non2(interaction: nextcord.Interaction, data3: str):
+
+#     url = 'http://127.0.0.1:5000/'
+
+#     response = requests.get(url)
+#     print(response.text)
+
+#     await interaction.send(f'{response.text}')
+
+# @bot.slash_command(name="fff")
+# async def non2(interaction: nextcord.Interaction, data3: str):
+
+#     payload = {'key1': 'test', 'key2': data3}
+#     headers = {'content-type': 'application/json'}
+#     url = 'http://127.0.0.1:5000/myapi'
+
+#     response = requests.post(url, data=json.dumps(payload), headers=headers)
+#     print(response.text)
+
+#     await interaction.send(f'{response.text}')
+    
+    
+# @bot.slash_command(name="ggg")
+# async def non2(interaction: nextcord.Interaction, data3: str):
+
+  
+#     url = 'http://127.0.0.1:5000/test'
+
+#     response = requests.get(url)
+#     print(response.text)
+
+#     await interaction.send(f'{response.text}')
+    
+    
+# @bot.slash_command(name="hhh")
+# async def non2(interaction: nextcord.Interaction, data1: str, data2: str, data3: str, data4: str):
+
+#     payload = {'Ticket': data1, 'DayStart': data2,'DayStop' : data3,'UniT' : data4}
+#     headers = {'content-type': 'application/json'}
+#     url = 'http://127.0.0.1:5000/myapi'
+
+#     response = requests.post(url, data=json.dumps(payload), headers=headers)
+#     print(response.text)
+
+#     await interaction.send(f'{response.text}')
+    
+# @bot.slash_command(name="iii")
+# async def non2(interaction: nextcord.Interaction, ticket: str, start: str, to: str, unit: str):
+
+#     payload = {'Ticket': ticket.upper(), 'DayStart': start,'DayStop' : to,'UniT' : unit}
+#     headers = {'content-type': 'application/json'}
+#     url = 'http://127.0.0.1:5000/test'
+
+#     response = requests.post(url, data=json.dumps(payload), headers=headers)
+#     print(response.text)
+
+#     await interaction.send(f'{response.text}')
+
+# ==================== Test Zone : End ====================
+    
+@bot.slash_command(name="price")
+async def non2(interaction: nextcord.Interaction, ticket: str, start: str, to: str, unit: str):
+
+    payload = {'Ticket': ticket.upper(), 'DayStart': start,'DayStop' : to,'UniT' : unit}
     headers = {'content-type': 'application/json'}
-    url = 'http://127.0.0.1:5000/'
-
-    response = requests.get(url, data=json.dumps(payload), headers=headers)
-    print(response.text)
-
-    await interaction.send(f'{response.text}')
-
-@bot.slash_command(name="fff")
-async def non2(interaction: nextcord.Interaction, data3: str):
-
-
-    payload = {'key1': 'value1', 'key2': 'value2'}
-    headers = {'content-type': 'application/json'}
-    url = 'http://127.0.0.1:5000/myapi'
+    url = 'http://127.0.0.1:5000/ask'
 
     response = requests.post(url, data=json.dumps(payload), headers=headers)
     print(response.text)
 
     await interaction.send(f'{response.text}')
+
+    
+@bot.event
+async def on_ready():
+    print(f'logged in as {bot.user}')
+
+
+if __name__ == '__main__':
+    bot.run(Token)
