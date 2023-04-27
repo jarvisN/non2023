809867fbcf855aceb5bbdf2343c8e9ac831cae86
Jarvis_N <non.ny1999@hotmail.com>
2023-04-27 19:21:42 +0700

    update : dev3FB

---
+++ python/08_FLN/test.py
@@ -0,0 +1,17 @@
+data = input("Order : ")
+
+print(data)
+
+if data == "w":
+    zumi.forward()
+
+elif data == "s":
+    zumi.reverse()    
+
+elif data == "a":
+    .... 
+elif data == "d"
+    ......
+else:
+    print("nothing")
+    zumi.stop()
