61207f64ecb819850ea9cbb03002b31131fb3d86
Jarvis_N <non.ny1999@hotmail.com>
2023-11-26 15:11:31 +0700

    update : web
    for train skill

---
+++ web/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from 'public' directory
+app.use(express.static('public'));
+// Body parser middleware to parse form data
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit-form', (req, res) => {
+  const { name, email, message } = req.body;
+  // Process form data here
+  console.log(name, email, message);
+  res.send('Form submitted');
+});
+
+app.post('/testData', (req, res) => {
+    const { username, password } = req.body;
+    // Process form data here
+    console.log(username, password);
+    res.send('Form submitted');
+  });
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
